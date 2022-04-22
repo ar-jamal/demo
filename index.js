21c061922ada08de9b0b208febd139b8865fb87f
Salman Karim <salmankarim.khan42@gmail.com>
2022-04-23 01:12:49 +0500

    add new line

--- index.js
+++ index.js
@@ -9,4 +9,5 @@ import {AppRegistry} from 'react-native';
 import App from './src/NavigationExercise';
 import {name as appName} from './app.json';
 
+
 AppRegistry.registerComponent(appName, () => App);
